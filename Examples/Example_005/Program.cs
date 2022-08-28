cabf8477879edf3c4ddbc2b37cef6e0a2f4d7b74
ubivakus <stepanov.alex.2012@gmail.com>
2022-08-28 21:57:27 +0300

    Example_005

---
+++ Examples/Example_005/Program.cs
@@ -0,0 +1,30 @@
+﻿int start = 50;
+int end = 100;
+
+for (int j = start; j <= end; j++)
+{
+    if (j % 2 == 0)
+    {
+        Console.Write(j);
+        if (j < end) // To avoid print , at the end
+        {
+            Console.Write(", ");
+        }
+
+    }
+}
+Console.WriteLine();
+for (int j = start; j <= end; j++)
+{
+    if (j % 2 != 0)
+    {
+        Console.Write(j);
+        if (j < end - 1)
+        {
+            Console.Write(", ");// To avoid print , at the end
+        }
+
+    }
+}
+Console.WriteLine();
+Console.ReadLine();
---
+++ Stepik/002/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+string line = Console.ReadLine();
+        string[] splitString = line.Split(' ');
+
+        int a = Convert.ToInt32(splitString[0]);
+        int b = Convert.ToInt32(splitString[1]);
+        int c = Convert.ToInt32(splitString[2]);
+        int d = Convert.ToInt32(splitString[3]);
+
+        int minValue = 0;
+        int maxValue = 0;
+
+        //Тут будет Ваш код
+        minValue = a;
+        maxValue = a;
+        if(a < b && a < c && a < d) minValue = a;
+        else if(b < a && b < c && b < d) minValue = b;
+        else if(c < a && c < b && c < d) minValue = c;
+        else if(d < a && d < b && d < c) minValue = d;
+
+        if(a > b && a > c && a > d) maxValue = a;
+        else if(b > a && b > c && b > d) maxValue = b;
+        else if(c > a && c > b && c > d) maxValue = c;
+        else if(d > a && d > b && d > c) maxValue = d;
+
+
+        Console.WriteLine($"Наименьшее число - {minValue}");
+        Console.WriteLine($"Наибольшее число - {maxValue}");
